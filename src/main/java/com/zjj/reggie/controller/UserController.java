5ee0a98eb00be2c8186f05ce629a1cf50beaca51
junjie-zou <819478523@qq.com>
2023-04-12 13:24:04 +0800

    v1.0 ---> redis缓存优化 ---> 缓存短信验证码

--- src/main/java/com/zjj/reggie/controller/UserController.java
+++ src/main/java/com/zjj/reggie/controller/UserController.java
@@ -7,6 +7,7 @@ import com.zjj.reggie.pojo.User;
 import com.zjj.reggie.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.mail.MessagingException;
 import javax.servlet.http.HttpSession;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @create: 2023-04-06 16:41
@@ -28,6 +30,9 @@ public class UserController {
     @Autowired
     UserService userService;
 
+    @Autowired
+    private RedisTemplate redisTemplate;
+
     @PostMapping("/sendMsg")
     public Result<String> sendMsg(@RequestBody User user, HttpSession session) throws MessagingException {
         String phone = user.getPhone();
@@ -37,8 +42,10 @@ public class UserController {
             log.info(code);
             //这里的phone其实就是邮箱，code是我们生成的验证码
             MailUtils.sendTestMail(phone, code);
-            //验证码存session，方便后面拿出来比对
-            session.setAttribute(phone, code);
+//            //验证码存session，方便后面拿出来比对
+//            session.setAttribute(phone, code);
+            //验证码缓存到Redis，设置存活时间5分钟
+            redisTemplate.opsForValue().set(phone, code,5, TimeUnit.MINUTES);
             return Result.success("验证码发送成功");
         }
         return Result.error("验证码发送失败");
@@ -51,11 +58,13 @@ public class UserController {
         // 获取验证码
         String code = map.get("code").toString();
 
-        // 从Session中获取验证码GroundTruth
-        String codeInSession = session.getAttribute(phone).toString();
+//        // 从Session中获取验证码GroundTruth
+//        String codeInSession = session.getAttribute(phone).toString();
+        // 改成从redis缓存中获取验证码
+        Object codeInRedis = redisTemplate.opsForValue().get(phone);
 
         //比较这用户输入的验证码和session中存的验证码是否一致
-        if (code != null && code.equals(codeInSession)) {
+        if (code != null && code.equals(codeInRedis)) {
             //如果输入正确，判断一下当前用户是否存在
             LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
             //判断依据是从数据库中查询是否有其邮箱
@@ -66,10 +75,15 @@ public class UserController {
                 user = new User();
                 user.setPhone(phone);
                 userService.save(user);
-                user.setName("用户" + codeInSession);
+//                user.setName("用户" + codeInSession);
+                user.setName("用户" + codeInRedis);
             }
             //存个session，表示登录状态
             session.setAttribute("user",user.getId());
+
+            //如果成功，则删除Redis中的验证码
+            redisTemplate.delete(phone);
+
             //并将其作为结果返回
             return Result.success(user);
         }
